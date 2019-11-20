82fa868788da3bdc862cdc6fd3508dd9a5372eb3
Tucker <trblackw@ncsu.edu>
2019-11-20 11:47:50 -0500

    init commit

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
